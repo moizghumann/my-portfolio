e42bfee997611a6195a48f0bb4ab635157d5e765
moizghumann <moizghuman@gmail.com>
2023-08-05 04:15:44 +0500

    fix hero heading size on mobile breakpoint

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,16 +3,20 @@ import Section from './Section';
 import Button from './Button';
 import Link from 'next/link';
 
+
 const Hero = () => {
     return (
         <Section>
-            <div className=' block mt-56 mb-28'>
-                <h1 className='text-7xl mx-auto text-center font-semibold max-w-4xl tracking-tight text-[#e6e1c5] opacity-90'>
+            <div className=' block mt-56 xs:mt-36 mb-28'>
+
+                <h1 className='text-7xl xs:text-4xl mx-auto text-center font-semibold max-w-4xl tracking-tight text-[#e6e1c5] opacity-90'>
                     Moiz focuses on crafting meaningful experiences as an
-                    <span className='text-[#CEFF1A] ml-4'>
+                    <span className='text-[#CEFF1A] ml-4 xs:ml-2'>
                         independent front-end developer
                     </span>
                 </h1>
+
+
                 <div className=' flex flex-row justify-center gap-4 mt-14'>
                     <Link href={'/blogs'}>
                         <Button>
