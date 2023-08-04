12aef08acbd1c0f4e1d45e4090f822ac646c9fda
moizghumann <moizghuman@gmail.com>
2023-08-05 02:03:46 +0500

    fix spacing + font size - motto section mobile bpt

--- components/Motto.tsx
+++ components/Motto.tsx
@@ -7,10 +7,10 @@ import Badge from './Badge'
 const Motto = () => {
     return (
         <Section>
-            <div className='  flex flex-col justify-center items-center h-screen mb-7'>
+            <div className='  grid place-items-center xl:h-screen xs:h-60vh mb-7 xs:mt-10 '>
                 <div className=' flex flex-col justify-center items-center w-9/12'>
                     <Badge badgeText='Motto' />
-                    <h1 className=' text-8xl font-semibold tracking-tight text-center mt-4'>
+                    <h1 className=' text-8xl xs:text-6xl font-semibold tracking-tight text-center mt-4'>
                         Design experiences that people never forget
                     </h1>
                 </div>
