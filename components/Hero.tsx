72e10a5bc34ca7c762030f0911daa352831fdc12
moizghumann <moizghuman@gmail.com>
2023-08-06 14:48:34 +0500

    responsive optimisation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Section from './Section';
 import Button from './Button';
 import Link from 'next/link';
+import { Balancer } from 'react-wrap-balancer';
 
 
 const Hero = () => {
@@ -9,11 +10,13 @@ const Hero = () => {
         <Section>
             <div className=' block mt-56 xs:mt-36 mb-28'>
 
-                <h1 className='text-7xl xs:text-4xl mx-auto text-center font-semibold max-w-4xl tracking-tight text-[#e6e1c5] opacity-90'>
-                    Moiz focuses on crafting meaningful experiences as an
-                    <span className='text-[#CEFF1A] ml-4 xs:ml-2'>
-                        independent front-end developer
-                    </span>
+                <h1 className='text-7xl xs:text-[40px] mx-auto text-center font-semibold max-w-4xl  tracking-tight text-[#e6e1c5] opacity-90'>
+                    <Balancer>
+                        Moiz focuses on crafting meaningful experiences as an
+                        <span className='text-[#CEFF1A] ml-4 xs:ml-2'>
+                            independent frontend developer
+                        </span>
+                    </Balancer>
                 </h1>
 
 
