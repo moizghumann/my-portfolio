226abb53201d84b17ef66cafa8853461393dad19
moizghumann <moizghuman@gmail.com>
2023-08-08 19:13:04 +0500

    fix about section tet size and grids on 425px bpt

--- components/About.tsx
+++ components/About.tsx
@@ -7,8 +7,8 @@ const About = () => {
     return (
         <Section>
 
-            <div className=' grid grid-cols-2 gap-8 mt-20 xs:gap-3 xss:gap-2 xs:mt-0 xs:grid-cols-4 xss:grid-cols-7 '>
-                <div className=' rounded-xl overflow-hidden relative xs:col-span-2 xss:col-span-4 '>
+            <div className=' grid grid-cols-2 gap-8 mt-20 xs:gap-3 xss:gap-2 xs:mt-0 xs:grid-cols-10 xss:grid-cols-7'>
+                <div className=' rounded-xl overflow-hidden relative xs:col-span-6 xss:col-span-4 '>
                     <Image src={anotherme}
                         alt='moiz'
                         fill={true}
@@ -17,12 +17,12 @@ const About = () => {
                         priority
                     />
                 </div>
-                <div className='flex flex-col justify-around items-start border-2 border-[#e6e1c5] rounded-xl p-5 xs:p-2 xss:p-2 min-w-fit '>
-                    <h1 className=' text-9xl xs:text-[62px] xss:text-5xl text-[#e6e1c5] font-semibold tracking-tight'>The one</h1>
-                    <h1 className=' text-9xl xs:text-[62px] xss:text-5xl text-[#e6e1c5] font-semibold tracking-tight'>Who</h1>
-                    <h1 className=' text-9xl xs:text-[62px] xss:text-5xl text-[#e6e1c5] font-semibold tracking-tight'>Gets</h1>
-                    <h1 className=' text-9xl xs:text-[62px] xss:text-5xl text-[#e6e1c5] font-semibold tracking-tight'>Shit</h1>
-                    <h1 className=' text-9xl xs:text-[62px] xss:text-5xl text-[#cbf420] font-semibold tracking-tight opacity-90'>Done!</h1>
+                <div className='flex flex-col justify-around items-start border-2 border-[#e6e1c5] rounded-xl p-5 xs:p-2 xs:col-span-4 xss:p-2 min-w-fit text-9xl xs:text-[55px] xss:text-5xl'>
+                    <h1 className='  text-[#e6e1c5] font-semibold tracking-tight'>The one</h1>
+                    <h1 className='  text-[#e6e1c5] font-semibold tracking-tight'>Who</h1>
+                    <h1 className='  text-[#e6e1c5] font-semibold tracking-tight'>Gets</h1>
+                    <h1 className='  text-[#e6e1c5] font-semibold tracking-tight'>Shit</h1>
+                    <h1 className='  text-[#cbf420] font-semibold tracking-tight opacity-90'>Done!</h1>
                 </div>
             </div>
 
