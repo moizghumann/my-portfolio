70b9add787093ee7c25424db9220a31380e272f4
moizghumann <moizghuman@gmail.com>
2023-08-08 21:23:24 +0500

    fix hero heading size on 758px bpt

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,9 +8,12 @@ import { Balancer } from 'react-wrap-balancer';
 const Hero = () => {
     return (
         <Section>
-            <div className=' block mt-56 xs:mt-36 xss:mt-36 mb-28 '>
+            <div className=' block mt-56 xs:mt-36 xss:mt-36 mb-28 
+            xt:h-screen xt:mb-0'>
 
-                <h1 className='text-7xl xs:text-[45px] xs:leading-[50px] xss:text-4xl mx-auto text-center font-semibold xss:font-bold max-w-4xl  tracking-tight text-[#e6e1c5] opacity-90'>
+                <h1 className='text-7xl xs:text-[45px] xs:leading-[50px] xss:text-4xl mx-auto text-center
+                xt:text-5xl 
+                font-semibold xss:font-bold max-w-4xl  tracking-tight text-[#e6e1c5] opacity-90'>
                     <Balancer>
                         Moiz focuses on crafting meaningful experiences as an
                         <span className='text-[#CEFF1A] ml-4 xs:ml-2 xss:ml-2'>
